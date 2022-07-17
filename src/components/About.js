e06fc98a109cd4c7d6d8a1a68059d42f152c0b9b
geoffreyogembo <geoffreyogembo06@gmail.com>
2022-07-16 20:36:25 -0400

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {!!props.bio && <p>{props.bio}</p>}
+      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
         <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links github={props.github} linkedin={props.linkedin} />
     </div>
